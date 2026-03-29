3adb0eeb714084e0d0c1125b545096b8f8a1255f
shyamthyagarajan <shyammuralithyagarajan@gmail.com>
2026-03-29 03:56:03 -0600

    Fixed issue with comet ephemeris trajectories, switched to json parsing in api_search

--- ephemeris/api_search.py
+++ ephemeris/api_search.py
@@ -1,4 +1,5 @@
 import urllib.request
+import json
 
 def query_horizons(id_string):
     if " " in id_string:
@@ -12,58 +13,36 @@ def query_horizons(id_string):
             html_content = html_bytes.decode('utf-8')
     except urllib.error.URLError as e:
         print(f"Error making request: {e.reason}")
+        return {}
 
-    no_match_identifier = '"count":0'
-    single_match_identifier = '"count":1'
-    asteroid_check_identifier = '"type":"asteroid'
+    parsed = json.loads(html_content)
+    total_cnt = parsed['count']
+    results = parsed['result']
 
     id_number = {}
 
-    if no_match_identifier in html_content:
+    if total_cnt == 0:
         pass
-    elif single_match_identifier in html_content:
-        if asteroid_check_identifier in html_content:
-            extract_substr = '"name":"'
-            start_index = html_content.find(extract_substr) + len(extract_substr)
-            end_index = html_content.find(' ', start_index)
-            id_number = html_content[start_index:end_index]
+    elif total_cnt == 1:
+        body = results[0]
+        if 'asteroid' in body['type']:
+            id_number = body['name'].split()[0]
+        elif 'comet' in body['type']:
+            id_number = f"DES={body['spkid']};"
         else:
-            extract_substr = '"spkid":"'
-            start_index = html_content.find(extract_substr) + len(extract_substr)
-            end_index = html_content.find('"', start_index)
-            id_number = html_content[start_index:end_index]
+            id_number = body['spkid']
     else:
-        extract_count = '"count":'
-        total_cnt_str_start_idx = html_content.rfind(extract_count) + len(extract_count)
-        total_cnt_str_idx = html_content.find(',', total_cnt_str_start_idx)
-        total_cnt = int(html_content[total_cnt_str_start_idx:total_cnt_str_idx])
-        
-        results_start = html_content.find('"result":[') + len('"result":[')
-        start_idx_cnt = results_start
-        id_number = {}
+        for body in results:
+            name = body['name']
+            if 'asteroid' in body['type']:
+                id_number[name] = body['name'].split()[0]
+            elif 'comet' in body['type']:
+                id_number[name] = f"DES={body['spkid']};"
+            else:
+                id_number[name] = body['spkid']
 
-        for i in range(total_cnt):
-            start_index = html_content.find("{", start_idx_cnt)
-            end_index = html_content.find("}", start_index)
-
-            html_content_substr = html_content[start_index:end_index]
-
-            extract_substr_name = '"name":"'
-            start_sub_idx = html_content_substr.find(extract_substr_name) + len(extract_substr_name)
-            end_sub_idx = html_content_substr.find('"', start_sub_idx)
-            id_number_name = html_content_substr[start_sub_idx:end_sub_idx]
-
-            extract_substr_spkid = '"spkid":"'
-            start_sub_idx = html_content_substr.find(extract_substr_spkid) + len(extract_substr_spkid)
-            end_sub_idx = html_content_substr.find('"', start_sub_idx)
-            id_number_spkid = html_content_substr[start_sub_idx:end_sub_idx]
-
-            id_number[id_number_name] = id_number_spkid
-            if i != total_cnt - 1:
-                start_idx_cnt = end_index + 1
-        
     return id_number
 
 if __name__ == "__main__":
-    result = query_horizons("Neptune")
+    result = query_horizons("Neowise")
     print(result)
