538298ff2ed0f8bf4e0003531e8508153c74a957
shyamthyagarajan <shyammuralithyagarajan@gmail.com>
2026-03-29 14:48:31 -0600

    Revert "Fixed issue with comet ephemeris trajectories, switched to json parsing in api_search"
    This reverts commit 3adb0eeb714084e0d0c1125b545096b8f8a1255f.

--- ephemeris/api_search.py
+++ ephemeris/api_search.py
@@ -1,9 +1,4 @@
 import urllib.request
-import json
-import ssl
-import certifi
-
-ctx = ssl.create_default_context(cafile=certifi.where())
 
 def query_horizons(id_string):
     if " " in id_string:
@@ -12,41 +7,63 @@ def query_horizons(id_string):
     url_begin_str = "https://ssd.jpl.nasa.gov/api/horizons_lookup.api?sstr="
     url = url_begin_str + id_string
     try:
-        with urllib.request.urlopen(url, context=ctx) as response:
+        with urllib.request.urlopen(url) as response:
             html_bytes = response.read()
             html_content = html_bytes.decode('utf-8')
     except urllib.error.URLError as e:
         print(f"Error making request: {e.reason}")
-        return {}
 
-    parsed = json.loads(html_content)
-    total_cnt = parsed['count']
-    results = parsed['result']
+    no_match_identifier = '"count":0'
+    single_match_identifier = '"count":1'
+    asteroid_check_identifier = '"type":"asteroid'
 
     id_number = {}
 
-    if total_cnt == 0:
+    if no_match_identifier in html_content:
         pass
-    elif total_cnt == 1:
-        body = results[0]
-        if 'asteroid' in body['type']:
-            id_number = body['name'].split()[0]
-        elif 'comet' in body['type']:
-            id_number = f"DES={body['spkid']};"
+    elif single_match_identifier in html_content:
+        if asteroid_check_identifier in html_content:
+            extract_substr = '"name":"'
+            start_index = html_content.find(extract_substr) + len(extract_substr)
+            end_index = html_content.find(' ', start_index)
+            id_number = html_content[start_index:end_index]
         else:
-            id_number = body['spkid']
+            extract_substr = '"spkid":"'
+            start_index = html_content.find(extract_substr) + len(extract_substr)
+            end_index = html_content.find('"', start_index)
+            id_number = html_content[start_index:end_index]
     else:
-        for body in results:
-            name = body['name']
-            if 'asteroid' in body['type']:
-                id_number[name] = body['name'].split()[0]
-            elif 'comet' in body['type']:
-                id_number[name] = f"DES={body['spkid']};"
-            else:
-                id_number[name] = body['spkid']
+        extract_count = '"count":'
+        total_cnt_str_start_idx = html_content.rfind(extract_count) + len(extract_count)
+        total_cnt_str_idx = html_content.find(',', total_cnt_str_start_idx)
+        total_cnt = int(html_content[total_cnt_str_start_idx:total_cnt_str_idx])
+        
+        results_start = html_content.find('"result":[') + len('"result":[')
+        start_idx_cnt = results_start
+        id_number = {}
 
+        for i in range(total_cnt):
+            start_index = html_content.find("{", start_idx_cnt)
+            end_index = html_content.find("}", start_index)
+
+            html_content_substr = html_content[start_index:end_index]
+
+            extract_substr_name = '"name":"'
+            start_sub_idx = html_content_substr.find(extract_substr_name) + len(extract_substr_name)
+            end_sub_idx = html_content_substr.find('"', start_sub_idx)
+            id_number_name = html_content_substr[start_sub_idx:end_sub_idx]
+
+            extract_substr_spkid = '"spkid":"'
+            start_sub_idx = html_content_substr.find(extract_substr_spkid) + len(extract_substr_spkid)
+            end_sub_idx = html_content_substr.find('"', start_sub_idx)
+            id_number_spkid = html_content_substr[start_sub_idx:end_sub_idx]
+
+            id_number[id_number_name] = id_number_spkid
+            if i != total_cnt - 1:
+                start_idx_cnt = end_index + 1
+        
     return id_number
 
 if __name__ == "__main__":
-    result = query_horizons("Neowise")
+    result = query_horizons("Neptune")
     print(result)
